fe97e0c70a6b9e8eedbced11b2561c2b1231e141
Fabian Schuh <xeroc@chainsquad.com>
2020-01-07 15:20:33 +0100

    Backwards compatibility
    I used to run powerdns-admin through `run.py` wrapped in a systemd service with configuration in `config.py`.
    Due to [https://github.com/ngoduykhanh/PowerDNS-Admin/blob/master/powerdnsadmin/__init__.py#L61](this code),
    the `config.py` in the project root would only be loaded in case it is passed to `create_app`.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from powerdnsadmin import create_app
 
+app = create_app("../config.py")
+    
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug = True, host=app.config.get('BIND_ADDRESS', '127.0.0.1'), port=app.config.get('PORT', '9191'))
